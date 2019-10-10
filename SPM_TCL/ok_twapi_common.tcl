1bdbe66b765568fe6a69f81cda8077e7c180cc57
oleglk <oleg.kosyakovsky@github.com>
2019-10-10 17:47:37 +0300

    Moved common utils into ok_twapi_common.tcl

---
+++ SPM_TCL/ok_twapi_common.tcl
@@ -0,0 +1,308 @@
+# ok_twapi_common.tcl - common utilities for TWAPI based automation
+
+package require twapi;  #  TODO: check errors
+
+namespace eval ::ok_twapi:: {
+
+  variable PID 0;       # pid of the singletone instance of StereoPhotoMaker
+  variable HWND "";     # TOP-LEVEL window handle of the controlled singletone
+  variable LATEST_APP_WND ""; # latest StereoPhotoMaker active window handle - top or child
+  
+  variable APP_NAME
+  variable APP_TOPWND_TITLE
+
+  
+  namespace export  \
+    # (DO NOT EXPORT:)  start_singleton  \
+    # (DO NOT EXPORT:)  fix_one_file
+}
+
+
+
+proc ::ok_twapi::start_singleton {exePath appName appWndTitle {workarea_rootdir ""}}  {
+  variable PID
+  variable HWND
+  variable APP_TOPWND_TITLE
+  variable APP_NAME
+
+  set APP_NAME $appName
+  set APP_TOPWND_TITLE $appWndTitle
+  set execDescr "invoking $APP_NAME"
+  if { 0 < [set PID [exec $exePath &]] }  {
+    puts "-I- Success $execDescr" } else {
+    puts "-E- Failed $execDescr";  return  0
+  }
+  set wndDescr "locating main window of $APP_NAME"
+  if { 0 < [set HWND [twapi::find_windows -text "$APP_TOPWND_TITLE" \
+                              -toplevel 1 -visible 1 -single]]  }  {
+    puts "-I- Success $wndDescr"
+    set_latest_app_wnd $HWND
+  } else {
+    puts "-E- Failed $wndDescr";  return  0
+  }
+
+  return  $HWND
+}
+
+
+proc ::ok_twapi::quit_singleton {cb__return_to_top}  {
+  variable APP_NAME
+  variable HWND;      # window handle of StereoPhotoMaker
+  set descr "quitting $APP_NAME instance"
+  if { ![verify_singleton_running $descr] }  {
+    return  1;  # OK; warning already printed
+  }
+  if { 1 == [focus_singleton "::spm::quit_singleton"] }  {
+    if { 1 == [$cb__return_to_top] }  {
+      twapi::send_keys {{MENU}fx};  # choose "exit" in "file" menu
+      after 200; # avoid an access denied error.
+      puts "-I- Success $descr"
+      set HWND 0;   forget_latest_app_wnd
+      return  1
+    }
+  }
+  puts "-E- Failed $descr"
+  return  0
+}
+
+
+# If 'targetHwnd' given, focuses it; otherwise focuses the latest SPM window
+proc ::ok_twapi::focus_singleton {context {targetHwnd 0}}  {
+  variable APP_NAME
+  variable LATEST_APP_WND;      # latest visited window handle of StereoPhotoMaker
+  set descr [expr {($context != "")? $context : \
+                                "giving focus to $APP_NAME instance"}]
+  if { ![verify_singleton_running $descr] }  {
+    return  0;  # warning already printed
+  }
+  if { $targetHwnd == 0 }  {
+    set targetHwnd $LATEST_APP_WND;   set explicitTarget 0
+  } else {                            set explicitTarget 1 }
+  twapi::set_focus $targetHwnd
+  after 200
+  set isOK [expr { ($explicitTarget == 1)? \
+                        ($targetHwnd == [twapi::get_foreground_window])   : \
+                        (1 == [is_current_window_related]) }]
+  if { $isOK == 1 }  {
+    puts "-I- Success $descr";    return  1
+  } else {
+    puts "-E- Failed $descr";     return  0
+  }
+}
+
+proc ::ok_twapi::get_top_app_wnd {}   {
+  variable HWND;      # window handle of the controlled app
+  return  $HWND
+}
+
+
+proc ::ok_twapi::set_latest_app_wnd_to_current {}  {
+  return  [set_latest_app_wnd [twapi::get_foreground_window]]
+}
+
+proc ::ok_twapi::set_latest_app_wnd {hwnd}  {
+  variable LATEST_APP_WND;      # latest visited window handle of StereoPhotoMaker
+  set LATEST_APP_WND $hwnd
+}
+
+
+proc ::ok_twapi::forget_latest_app_wnd {}  {
+  variable LATEST_APP_WND;      # latest visited window handle of StereoPhotoMaker
+  set LATEST_APP_WND ""
+}
+
+
+proc ::ok_twapi::verify_singleton_running {contextDescr}  {
+  variable APP_NAME
+  variable HWND;      # window handle of StereoPhotoMaker
+  if { $HWND == "" }  {
+    if { $contextDescr != "" }  {
+      puts "-W- $APP_NAME instance not running; context: $contextDescr"
+    }
+    return  0
+  }
+  return  1
+}
+
+
+proc ::ok_twapi::verify_current_window_by_title {title {loud 1}}  {
+  set h  [twapi::get_foreground_window]
+  set txt [twapi::get_window_text $h]
+  if { $txt != $title } {
+    if { $loud }  {
+      puts "-I- Unexpected foreground window '$txt' instead of '$title'"
+    }
+    return  0
+  }
+  return  1
+}
+
+
+# Returns 1 if the current foreground window is the controlled app top or its descendant
+proc ::ok_twapi::is_current_window_related {} {
+  variable APP_NAME
+  variable HWND;      # window handle of StereoPhotoMaker
+  variable LATEST_APP_WND
+  set descr [lindex [info level 0] 0]
+  if { ![verify_singleton_running $descr] }  {
+    return  0;  # warning already printed
+  }
+  set h [twapi::get_foreground_window];  set txt [twapi::get_window_text $h]
+  set isIt [expr {($h == $HWND) || ($h == $LATEST_APP_WND)}]  
+  set doesOrNot [expr {$isIt ?  "does" : "does not"}]
+  # puts "-D- Window '$txt' $doesOrNot belong to SPM application"
+  puts "-D- Window '$txt' ($h) $doesOrNot belong to $APP_NAME application"
+  return  $isIt
+  ### Approaches that DO NOT WORK:
+  ### (1) Comparing [twapi::get_window_application [twapi::get_foreground_window]]
+  ###         with  [twapi::get_window_application $::spm::HWND]
+  ### (2) Traversing windows upwards using [twapi::get_parent_window] until top
+  ##
+  #~ set isIt [expr {[twapi::get_window_application $h] == \
+                                #~ [twapi::get_window_application $HWND]}]
+  ##
+  #~ # can be child- or top window; go up until top reached
+  #~ while { ($h != "") && ($h != $HWND) }    {
+    #~ puts "-D- $descr passed window $h ([twapi::get_window_text $h]) while searching for '$HWND' ([twapi::get_window_text $HWND])"
+    #~ set h [twapi::get_parent_window $h]
+  #~ }
+  #~ puts "-D- ending is_current_window_related with handle '$h' (HWND == '$HWND')"
+  #~ return  [expr {$h == $HWND}]
+}
+
+
+proc ::ok_twapi::cmd__maximize_current_window {} {
+  set h [twapi::get_foreground_window]
+  # can be child- or top window
+  set descr "maximize window '[twapi::get_window_text $h]'";   # [lindex [info level 0] 0]
+  if { 1 == [focus_singleton "focus for $descr" $h] }  {
+    puts "-I- Commanding to $descr"
+    set wasVisible [twapi::maximize_window $h -sync];  # succeeds or gets stuck
+    return  1
+    #~ if { 0 != [_send_cmd_keys "{MENU}{SPACE}" $descr $h] }  {
+      #~ return  [expr { [_send_cmd_keys {x} $descr 0] }]
+    #~ }
+  }
+  return  0;  # error already printed
+}
+
+# Safely opens 1st level of the menu for key 'oneKey'
+proc ::ok_twapi::open_menu_top_level {oneKey descr} {
+  variable HWND;      # window handle of StereoPhotoMaker
+  ## TODO: ??? ENSURE CHILD-WINDOWS CLOSED BY PRESSING {ESC} UNTILL TOP ???
+  ######set res [44 [format "{MENU}%s" $oneKey] $descr $HWND]
+  if { 1 == [set res [focus_singleton "focus for $descr" $HWND]] }  {
+    #### _send_timed_keys_list [list {MENU} [format "%s" $oneKey]] 2000
+    twapi::send_keys {{MENU}}
+    after 2000;  # wait A LOT after ALT
+    twapi::send_keys [list $oneKey]
+    after 1000
+  }
+  return  $res
+}
+
+
+###################### Begin: subtask utilities ################################
+
+# Sends menu shortcut keys to travel pre-open menu
+# Returns handle of resulting window or "" on error.
+proc  ::ok_twapi::_travel_meny_hierarchy {keySeqStr descr {targetWndTitle ""}}  {
+  if { ("" == [set h [_send_cmd_keys $keySeqStr $descr 0]]) }  {
+    return  "";  # error already printed
+  }
+  if { $targetWndTitle == "" }  { return  $h }; # done; no verification requested
+  set h [_wait_for_window_title_to_raise $targetWndTitle]
+  set wndText [expr {($h != "")? [twapi::get_window_text $h] : "NONE"}]
+  puts "-D- Key sequence '$keySeqStr' lead to window '$wndText'"
+  return  [expr {($wndText == $targetWndTitle)? $h : ""}]
+}
+
+
+# Waits with active polling
+proc ::ok_twapi::_wait_for_window_title_to_raise {titleStr}  {
+  return  [_wait_for_window_title_to_raise__configurable $titleStr 500 5000]
+}
+
+
+# Waits with active polling - configurable
+# Returns handle of resulting window or "" on error.
+proc ::ok_twapi::_wait_for_window_title_to_raise__configurable { \
+                                        titleStr pollPeriodMsec maxWaitMsec}  {
+  if { $titleStr == "" }  {
+    puts "-E- No title provided for [lindex [info level 0] 0]";   return  ""
+  }
+  set nAttempts [expr {int( ceil(1.0 * $maxWaitMsec / $pollPeriodMsec) )}]
+  if { $nAttempts == 0 }  { set nAttempts 1 }
+  ### after 2000 ;  # unfortunetly need to wait
+  for {set i 1} {$i <= $nAttempts} {incr i 1}   {
+    if { 1 == [verify_current_window_by_title $titleStr] }  {
+      set h [twapi::get_foreground_window]
+      if { ($h != "") && (1 == [twapi::window_visible $h]) }  {
+        puts "-I- Window '$titleStr' did appear after [expr {$i * $pollPeriodMsec}] msec"
+        return  $h
+      }
+    }
+    after $pollPeriodMsec
+  }
+  puts "-E- Window '$titleStr' did not appear after [expr {$nAttempts * $pollPeriodMsec}] msec"
+  return  ""
+}
+
+
+# Sends given keys while taking care of occurences of {MENU}.
+# If 'targetHwnd' given, first focuses this window
+# Returns handle of resulting window or 0 on error.
+# TODO: The sequence of {press-Alt, release-Alt, press-Cmd-Key} is not universal
+proc ::ok_twapi::_send_cmd_keys {keySeqStr descr {targetHwnd 0}} {
+  set descr "sending key-sequence {$keySeqStr} for '$descr'"
+  set subSeqList [_split_key_seq_at_alt $keySeqStr]
+  if { ($targetHwnd == 0) || \
+        (1 == [focus_singleton "focus for $descr" $targetHwnd]) }  {
+    after 1000
+    if { 0 == [llength $subSeqList] }   {
+      twapi::send_keys $keySeqStr
+     } else {
+      foreach subSeq $subSeqList  {
+        twapi::send_keys {{MENU}}
+        after 1000;  # wait A LOT after ALT
+        twapi::send_keys $subSeq
+      }
+     }
+    after 200; # avoid an access denied error
+    puts "-I- Success $descr";      return  [twapi::get_foreground_window]
+  }
+  puts "-E- Cannot $descr";         return  0
+}
+
+
+# Sends given keys while taking care of occurences of {MENU}.
+# If 'targetHwnd' given, first focuses this window
+# Returns handle of resulting window or 0 on error.
+proc ::ok_twapi::_send_timed_keys_list {keysList descr {intervalMiliSec 0}} {
+  set descr "sending keys-list {$keysList} with interval $intervalMiliSec msec for '$descr'"
+  foreach k $keysList {
+    twapi::send_keys [list $k]
+    if { $intervalMiliSec != 0 }    {
+      after $intervalMiliSec
+    }
+  }
+  puts "-I- Done $descr"
+}
+
+
+# Returns list of subsequences that follow occurences of {MENU}/{ALT}
+# In the case of no occurences of {MENU}/{ALT}, returns empty list
+proc ::ok_twapi::_split_key_seq_at_alt {keySeqStr} {
+  # the idea:  set list [split [string map [list $substring $splitchar] $string] $splitchar]
+  set tmp [string map {\{MENU\} \uFFFF  \{ALT\} \uFFFF} $keySeqStr]
+  if { [string equal $tmp $keySeqStr] }   {
+    return  [list];   # no occurences of {MENU}/{ALT}
+  }
+  set tmpList [split $tmp \uFFFF];  # may have empty elements
+  set subSeqList [list]
+  foreach el $tmpList {
+    if { $el != "" }  { lappend subSeqList $el }
+  }
+  return  $subSeqList
+}
+######################   End: subtask utilities ################################
--- SPM_TCL/spm_basics.tcl
+++ SPM_TCL/spm_basics.tcl
@@ -8,6 +8,9 @@
 
 package require twapi;  #  TODO: check errors
 
+set SCRIPT_DIR [file dirname [info script]]
+source [file join $SCRIPT_DIR "ok_twapi_common.tcl"]
+
 namespace eval ::spm:: {
   ### variable ORIG_PATTERN {*.tif}
   variable ORIG_PATTERN {*.jpg}
@@ -15,11 +18,8 @@ namespace eval ::spm:: {
   variable SUBDIR_INP "FIXED";  # subdirectory for to-be-aligned images
   variable SUBDIR_PRE "Pre";    # subdirectory for pre-aligned images
   
-  variable SPM_TITLE  "StereoPhoto Maker"
+  variable SPM_TITLE  "StereoPhoto Maker" ;   # title of the main SPM window
   
-  variable PID 0;       # pid of the singletone instance of StereoPhotoMaker
-  variable HWND 0;      # TOP-LEVEL window handle of StereoPhotoMaker
-  variable LATEST_SPM_WND 0; # latest StereoPhotoMaker active window handle - top or child
   variable WA_ROOT "";  # work-area root directory
 
   
@@ -29,9 +29,7 @@ namespace eval ::spm:: {
 }
 
 
-proc ::spm::start_singleton {{workarea_rootdir ""}}  {
-  variable PID
-  variable HWND;  variable LATEST_SPM_WND
+proc ::spm::start_spm {{workarea_rootdir ""}}  {
   variable WA_ROOT
   variable SPM_TITLE
   if { $workarea_rootdir != "" }  {
@@ -42,154 +40,33 @@ proc ::spm::start_singleton {{workarea_rootdir ""}}  {
     set WA_ROOT [file normalize $workarea_rootdir]
     puts "-I- Workarea root directory set to '$WA_ROOT'"
   }
-  set execDescr "invoking StereophotoMaker"
-  if { 0 < [set PID [exec $::SPM &]] }  {
-    puts "-I- Success $execDescr" } else {
-    puts "-E- Failed $execDescr";  return  0
-  }
-  set wndDescr "locating main window of StereoPhotoMaker"
-  if { 0 < [set HWND [twapi::find_windows -text "$SPM_TITLE" \
-                              -toplevel 1 -visible 1 -single]]  }  {
-    puts "-I- Success $wndDescr"
-    set LATEST_SPM_WND $HWND
-  } else {
-    puts "-E- Failed $wndDescr";  return  0
-  }
-
-  return  $HWND
-}
-
-proc ::spm::quit_singleton {}  {
-  variable PID;       # pid of the singletone instance of StereoPhotoMaker
-  variable HWND;      # window handle of StereoPhotoMaker
-  variable LATEST_SPM_WND
-  set descr "quitting StereoPhotoMaker instance"
-  if { ![verify_singleton_running $descr] }  {
-    return  1;  # OK; warning already printed
-  }
-  if { 1 == [focus_singleton "::spm::quit_singleton"] }  {
-    if { 1 == [cmd__return_to_top] }  {
-      twapi::send_keys {{MENU}fx};  # choose "exit" in "file" menu
-      after 200; # avoid an access denied error.
-      puts "-I- Success $descr"
-      set HWND 0;   set LATEST_SPM_WND 0
-      return  1
-    }
-  }
-  puts "-E- Failed $descr"
-  return  0
+  return  [::ok_twapi::start_singleton $::SPM \
+                "StereoPhotoMaker" $SPM_TITLE $workarea_rootdir]
 }
 
 
-# If 'targetHwnd' given, focuses it; otherwise focuses the latest SPM window
-proc ::spm::focus_singleton {context {targetHwnd 0}}  {
-  variable LATEST_SPM_WND;      # latest visited window handle of StereoPhotoMaker
-  set descr [expr {($context != "")? $context : \
-                                "giving focus to StereoPhotoMaker instance"}]
-  if { ![verify_singleton_running $descr] }  {
-    return  0;  # warning already printed
-  }
-  if { $targetHwnd == 0 }  {
-    set targetHwnd $LATEST_SPM_WND;   set explicitTarget 0
-  } else {                            set explicitTarget 1 }
-  twapi::set_focus $targetHwnd
-  after 200
-  set isOK [expr { ($explicitTarget == 1)? \
-                        ($targetHwnd == [twapi::get_foreground_window])   : \
-                        (1 == [is_current_window_spm]) }]
-  if { $isOK == 1 }  {
-    puts "-I- Success $descr";    return  1
-  } else {
-    puts "-E- Failed $descr";     return  0
-  }
-}
-
-
-proc ::spm::verify_singleton_running {contextDescr}  {
-  variable HWND;      # window handle of StereoPhotoMaker
-  if { $HWND == 0 }  {
-    if { $contextDescr != "" }  {
-      puts "-W- StereoPhotoMaker instance not running; context: $contextDescr"
-    }
-    return  0
-  }
-  return  1
-}
-
-
-proc ::spm::verify_current_window_by_title {title {loud 1}}  {
-  set h  [twapi::get_foreground_window]
-  set txt [twapi::get_window_text $h]
-  if { $txt != $title } {
-    if { $loud }  {
-      puts "-I- Unexpected foreground window '$txt' instead of '$title'"
-    }
-    return  0
-  }
-  return  1
+proc ::spm::quit_spm {}  {
+  return  [::ok_twapi::quit_singleton cmd__return_to_top]
 }
 
 
 # Returns 1 if the current foreground window is SPM-top or its descendant
 proc ::spm::is_current_window_spm {} {
-  variable HWND;      # window handle of StereoPhotoMaker
-  variable LATEST_SPM_WND
-  set descr [lindex [info level 0] 0]
-  if { ![verify_singleton_running $descr] }  {
-    return  0;  # warning already printed
-  }
-  set h [twapi::get_foreground_window];  set txt [twapi::get_window_text $h]
-  set isSPM [expr {($h == $HWND) || ($h == $LATEST_SPM_WND)}]  
-  set doesOrNot [expr {$isSPM ?  "does" : "does not"}]
-  # puts "-D- Window '$txt' $doesOrNot belong to SPM application"
-  puts "-D- Window '$txt' ($h) $doesOrNot belong to SPM application"
-  return  $isSPM
-  ### Approaches that DO NOT WORK:
-  ### (1) Comparing [twapi::get_window_application [twapi::get_foreground_window]]
-  ###         with  [twapi::get_window_application $::spm::HWND]
-  ### (2) Traversing windows upwards using [twapi::get_parent_window] until top
-  ##
-  #~ set isSPM [expr {[twapi::get_window_application $h] == \
-                                #~ [twapi::get_window_application $HWND]}]
-  ##
-  #~ # can be child- or top window; go up until top reached
-  #~ while { ($h != "") && ($h != $HWND) }    {
-    #~ puts "-D- $descr passed window $h ([twapi::get_window_text $h]) while searching for '$HWND' ([twapi::get_window_text $HWND])"
-    #~ set h [twapi::get_parent_window $h]
-  #~ }
-  #~ puts "-D- ending is_current_window_spm with handle '$h' (HWND == '$HWND')"
-  #~ return  [expr {$h == $HWND}]
-}
-
-
-proc ::spm::cmd__maximize_current_window {} {
-  set h [twapi::get_foreground_window]
-  # can be child- or top window
-  set descr "maximize window '[twapi::get_window_text $h]'";   # [lindex [info level 0] 0]
-  if { 1 == [focus_singleton "focus for $descr" $h] }  {
-    puts "-I- Commanding to $descr"
-    set wasVisible [twapi::maximize_window $h -sync];  # succeeds or gets stuck
-    return  1
-    #~ if { 0 != [_send_cmd_keys "{MENU}{SPACE}" $descr $h] }  {
-      #~ return  [expr { [_send_cmd_keys {x} $descr 0] }]
-    #~ }
-  }
-  return  0;  # error already printed
+  return  [::ok_twapi::is_current_window_related]
 }
 
 
 proc ::spm::cmd__return_to_top {} {
-  variable HWND;      # window handle of StereoPhotoMaker
-  variable LATEST_SPM_WND
   set descr "reach SPM top";  # [lindex [info level 0] 0]
-  if { 0 == [focus_singleton "focus to $descr" 0] }  {
+  if { 0 == [::ok_twapi::focus_singleton "focus to $descr" 0] }  {
     return  0;  # warning already printed
   }
   set nAttempts 30
   for {set i 1} {$i <= $nAttempts} {incr i 1}  {
-    if { $HWND == [set h [twapi::get_foreground_window]] }   {
+    set topWnd [ok::twapi::get_top_app_wnd]
+    if { $topWnd == [set h [twapi::get_foreground_window]] }   {
       puts "-I- Success to $descr after $i hit(s) of ESCAPE"
-      set LATEST_SPM_WND $HWND
+      ::ok_twapi::set_latest_app_wnd $topWnd
       return  1
     }
     puts "-I- Pressing ESCAPE ($i of $nAttempts) to $descr"
@@ -203,13 +80,11 @@ proc ::spm::cmd__return_to_top {} {
 
 # Returns handle of resulting window or "" on error.
 proc spm::cmd__open_multi_conversion {} {
-  variable HWND;      # window handle of StereoPhotoMaker
-  variable LATEST_SPM_WND
   set descr [lindex [info level 0] 0]
   #twapi::block_input
-  # _send_cmd_keys {{MENU}f} $descr $HWND
-  _open_menu_top_level "f" $descr;  # TODO: VERIFY SUCCESS"
-  if { 0 == [_travel_meny_hierarchy {{m 2}{ENTER}} \
+  # _send_cmd_keys {{MENU}f} $descr [::ok_twapi::get_top_app_wnd]
+  ::ok_twapi::open_menu_top_level "f" $descr;  # TODO: VERIFY SUCCESS"
+  if { 0 == [::ok_twapi::travel_meny_hierarchy {{m 2}{ENTER}} \
                                       $descr "Multi Conversion"] }  {
     #twapi::unblock_input
     return  "";  # error already printed
@@ -219,130 +94,9 @@ proc spm::cmd__open_multi_conversion {} {
     return  "";  # error already printed
   }
   #twapi::unblock_input
-  return  [set LATEST_SPM_WND [twapi::get_foreground_window]]
+  return  [::ok_twapi::set_latest_app_wnd_to_current]
 }
 
-
-###################### Begin: subtask utilities ################################
-
-# Safely opens 1st level of the menu for key 'oneKey'
-proc spm::_open_menu_top_level {oneKey descr} {
-  variable HWND;      # window handle of StereoPhotoMaker
-  ## TODO: ??? ENSURE CHILD-WINDOWS CLOSED BY PRESSING {ESC} UNTILL TOP ???
-  ######set res [44 [format "{MENU}%s" $oneKey] $descr $HWND]
-  if { 1 == [set res [focus_singleton "focus for $descr" $HWND]] }  {
-    #### _send_timed_keys_list [list {MENU} [format "%s" $oneKey]] 2000
-    twapi::send_keys {{MENU}}
-    after 2000;  # wait A LOT after ALT
-    twapi::send_keys [list $oneKey]
-    after 1000
-  }
-  return  $res
-}
-
-
-# Sends menu shortcut keys to travel pre-open menu
-# Returns handle of resulting window or "" on error.
-proc  ::spm::_travel_meny_hierarchy {keySeqStr descr {targetWndTitle ""}}  {
-  if { ("" == [set h [_send_cmd_keys $keySeqStr $descr 0]]) }  {
-    return  "";  # error already printed
-  }
-  if { $targetWndTitle == "" }  { return  $h }; # done; no verification requested
-  set h [_wait_for_window_title_to_raise "Multi Conversion"]
-  set wndText [expr {($h != "")? [twapi::get_window_text $h] : "NONE"}]
-  puts "-D- Key sequence '$keySeqStr' lead to window '$wndText'"
-  return  [expr {($wndText == $targetWndTitle)? $h : ""}]
-}
-
-
-# Waits with active polling
-proc ::spm::_wait_for_window_title_to_raise {titleStr}  {
-  return  [_wait_for_window_title_to_raise__configurable $titleStr 500 5000]
-}
-
-
-# Waits with active polling - configurable
-# Returns handle of resulting window or "" on error.
-proc ::spm::_wait_for_window_title_to_raise__configurable { \
-                                        titleStr pollPeriodMsec maxWaitMsec}  {
-  if { $titleStr == "" }  {
-    puts "-E- No title provided for [lindex [info level 0] 0]";   return  ""
-  }
-  set nAttempts [expr {int( ceil(1.0 * $maxWaitMsec / $pollPeriodMsec) )}]
-  if { $nAttempts == 0 }  { set nAttempts 1 }
-  ### after 2000 ;  # unfortunetly need to wait
-  for {set i 1} {$i <= $nAttempts} {incr i 1}   {
-    if { 1 == [verify_current_window_by_title $titleStr] }  {
-      set h [twapi::get_foreground_window]
-      if { ($h != "") && (1 == [twapi::window_visible $h]) }  {
-        puts "-I- Window '$titleStr' did appear after [expr {$i * $pollPeriodMsec}] msec"
-        return  $h
-      }
-    }
-    after $pollPeriodMsec
-  }
-  puts "-E- Window '$titleStr' did not appear after [expr {$nAttempts * $pollPeriodMsec}] msec"
-  return  ""
-}
-
-
-
-# Sends given keys while taking care of occurences of {MENU}.
-# If 'targetHwnd' given, first focuses this window
-# Returns handle of resulting window or 0 on error.
-proc ::spm::_send_cmd_keys {keySeqStr descr {targetHwnd 0}} {
-  set descr "sending key-sequence {$keySeqStr} for '$descr'"
-  set subSeqList [_split_key_seq_at_alt $keySeqStr]
-  if { ($targetHwnd == 0) || \
-        (1 == [focus_singleton "focus for $descr" $targetHwnd]) }  {
-    after 1000
-    if { 0 == [llength $subSeqList] }   {
-      twapi::send_keys $keySeqStr
-     } else {
-      foreach subSeq $subSeqList  {
-        twapi::send_keys {{MENU}}
-        after 1000;  # wait A LOT after ALT
-        twapi::send_keys $subSeq
-      }
-     }
-    after 200; # avoid an access denied error
-    puts "-I- Success $descr";      return  [twapi::get_foreground_window]
-  }
-  puts "-E- Cannot $descr";         return  0
-}
-
-
-# Sends given keys while taking care of occurences of {MENU}.
-# If 'targetHwnd' given, first focuses this window
-# Returns handle of resulting window or 0 on error.
-proc ::spm::_send_timed_keys_list {keysList descr {intervalMiliSec 0}} {
-  set descr "sending keys-list {$keysList} with interval $intervalMiliSec msec for '$descr'"
-  foreach k $keysList {
-    twapi::send_keys [list $k]
-    if { $intervalMiliSec != 0 }    {
-      after $intervalMiliSec
-    }
-  }
-  puts "-I- Done $descr"
-}
-
-
-# Returns list of subsequences that follow occurences of {MENU}/{ALT}
-# In the case of no occurences of {MENU}/{ALT}, returns empty list
-proc ::spm::_split_key_seq_at_alt {keySeqStr} {
-  # the idea:  set list [split [string map [list $substring $splitchar] $string] $splitchar]
-  set tmp [string map {\{MENU\} \uFFFF  \{ALT\} \uFFFF} $keySeqStr]
-  if { [string equal $tmp $keySeqStr] }   {
-    return  [list];   # no occurences of {MENU}/{ALT}
-  }
-  set tmpList [split $tmp \uFFFF];  # may have empty elements
-  set subSeqList [list]
-  foreach el $tmpList {
-    if { $el != "" }  { lappend subSeqList $el }
-  }
-  return  $subSeqList
-}
-######################   End: subtask utilities ################################
 
 
 proc ::spm::align_all {}  {
