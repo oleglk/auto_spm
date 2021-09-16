b3d5c14b96dcae7e3f4825c7b046e68cca820382
oleglk <oleg.kosyakovsky@gmail.com>
2021-09-16 16:00:04 +0300

    Example for curve application by Imagemagick

--- SPM_TCL/ok_utils/polynomial_fit.tcl
+++ SPM_TCL/ok_utils/polynomial_fit.tcl
@@ -70,11 +70,15 @@ proc ::ok_utils::_build.vector {xvec yvec degree} {
 
 
 # Returns list of coefficients starting from the lowest; on error returns ERROR
-## Example:
+## Example 1:
 ## set x {0   1   2   3   4   5   6   7   8   9  10};  set y {1   6  17  34  57  86 121 162 209 262 321}
 ## set xyDict [concat {*}[lmap a $x  b $y  {list $a $b}]]
 ## set coeffsLowToHigh [ok_utils::ok_fit_curve $xyDict 2]
 ## ok_utils::ok_format_curve_fitting $xyDict $coeffsLowToHigh
+## Example 2 (lower control point raised by 50%):
+##   set xTOy {0 0  0.25 0.375  0.5 0.5  1 1};  ok_utils::ok_fit_curve $xTOy 2
+### Example of curve application:
+### foreach f [glob -nocomplain {INP/*.JPG}]  {[string trim $IMCONVERT "{}"] $f -function Polynomial  -0.1818,1.1455,0.0273 [file join LIGHTEN POLY_50 [file tail $f]]}
 proc ::ok_utils::ok_fit_curve {xyDict {degree -1}}  {
   if { $degree == -1 }  { set degree [expr [dict size $xyDict] - 1] }
   if { [dict size $xyDict] < ($degree + 1) }  {
@@ -113,6 +117,8 @@ proc ::ok_utils::ok_format_curve_fitting {xyDict coeffsLowToHigh {degree -1}}  {
     }
     if { $expression != "" }  {
       append expression [expr {($coeff>=0)? " + "  :  " - "}]
+    } elseif { $coeff < 0 }  {
+      append expression "-"
     }
     append expression [format "%.4f%s" [expr abs($coeff)]                 \
                                        [expr {($iDeg>0)? "*x^$iDeg" : ""}]]
